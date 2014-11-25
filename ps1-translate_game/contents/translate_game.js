6520d08cee73c36db351de1f5e6afccef4f0602c
Jordan Arnesen <jordan101112@gmail.com>
2014-11-25 13:32:40 -0800

    getting target answer word pairs

--- ps1-translate_game/contents/translate_game.js
+++ ps1-translate_game/contents/translate_game.js
@@ -5,8 +5,24 @@ $(function() {
 	var lang_from		= "Spanish";
 	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
 
+
 	// Your code here
 	$(".from-lang").html(lang_from);
 	$(".to-lang").html(lang_to);
 
+	var target_word = "fzzzzt";
+	var answer_word = "fart";
+
+	var target_words = $.map(current_dict, function (value, key) { return value; });
+	var answer_words = $.map(current_dict, function (value, key) { return key; });
+
+    var randomNumber = [Math.floor(Math.random() * answer_words.length)];
+
+	target_word = target_words[randomNumber];
+	answer_word = answer_words[randomNumber];
+
+	$("#target-word").html(target_word);
+
     });
+
+
