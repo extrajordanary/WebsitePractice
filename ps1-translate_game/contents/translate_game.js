e86ddcdc5da73a2f3ff4962a49c9dd57a68d5940
Jordan Arnesen <jordan101112@gmail.com>
2014-11-25 14:06:20 -0800

    both button and hitting enter reset the input and generate a new target

--- ps1-translate_game/contents/translate_game.js
+++ ps1-translate_game/contents/translate_game.js
@@ -1,29 +1,53 @@
+var lang_to		= "English";
+var lang_from		= "Spanish";
+var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
+var target_words, answer_words, target_word, answer_word;
+
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $(function() {
-	var lang_to		= "English";
-	var lang_from		= "Spanish";
-	var current_dict	= dicts[lang_to][lang_from]; // keys: words in @lang_to, values: corresponding words in @lang_from 	
-
-
 	// Display the selected languages
 	$(".from-lang").html(lang_from);
 	$(".to-lang").html(lang_to);
 
+	updateWordArrays();
+	newQuestion();
+
+	// clear input, refocus, change target when clicked
+	$("button").click(function() {
+		newQuestion();
+	});
+
+});
+
+function updateWordArrays() {
+	// create arrays for the selected language dictionaries
+	target_words = $.map(current_dict, function (value, key) { return value; });
+	answer_words = $.map(current_dict, function (value, key) { return key; });
+	return 1; 
+}
+
+function updateWordPair() {
 	// get random target-answer pairs
-	var target_words = $.map(current_dict, function (value, key) { return value; });
-	var answer_words = $.map(current_dict, function (value, key) { return key; });
-
     var randomNumber = [Math.floor(Math.random() * answer_words.length)];
+	target_word = target_words[randomNumber];
+	answer_word = answer_words[randomNumber];
+}
 
-	var target_word = target_words[randomNumber];
-	var answer_word = answer_words[randomNumber];
+function resetInput() {
+	var input = $("input");
+	// clear the field
+	input.val("");
+	// set the focus on the input
+	input.focus();
+}
+
+function newQuestion() {
+	updateWordPair();
 
 	$("#target-word").html(target_word);
 
-	// set the focus on the input
-	$("input").focus();
-
-    });
+	resetInput();
+}
 
 
